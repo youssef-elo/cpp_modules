45b6d933e8dc3e4f4b2fdc0df9071362ee80de95
youssef-elo <144487581+youssef-elo@users.noreply.github.com>
2025-08-26 14:29:19 +0100

    cpp08 done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,14 +1,32 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack {
-	private:
+#include <stack>
 
+template <typename Type>
+class MutantStack : public std::stack<Type>
+{
 	public:
-		MutantStack();
-		~MutantStack();
-		MutantStack(const MutantStack&);
-		MutantStack& operator=(const MutantStack&);
+		typedef typename std::stack< Type >::container_type::iterator iterator;
+		virtual ~MutantStack() {}
+		MutantStack() : std::stack<Type>() {}
+		MutantStack(const MutantStack &other) : std::stack<Type>( other ) {}
+		MutantStack &operator=(const MutantStack &other)
+		{
+			if (this == &other)
+				return *this;
+			std::stack<Type>::operator=(other);
+			return *this;
+		}
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+		iterator end()
+		{
+			return this->c.end();
+		}
+
 };
 
 #endif
