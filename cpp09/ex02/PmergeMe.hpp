2ac48584d80bcdc1aec268e4a9cb122600d91bcb
youssef-elo <144487581+youssef-elo@users.noreply.github.com>
2025-09-20 18:02:13 +0100

    added the first part of the ford jhonson sort

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -4,109 +4,120 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 class PmergeMe {
 	private:
-
+		typedef std::vector<int>::iterator vec_iterator;
 	public:
-		// PmergeMe();
-		// ~PmergeMe();
-		// PmergeMe(const PmergeMe& other);
-		// PmergeMe& operator=(const PmergeMe& other);
+	
 		std::vector<size_t> jacob_sequence;
 		void generate_jacob_sequence( size_t n )
 		{
 			if ( n < jacob_sequence.size())
 				return;
-			if ( jacob_sequence.size() < 4)
+			if ( jacob_sequence.size() < 3)
 			{
-				jacob_sequence.push_back(0);
-				jacob_sequence.push_back(1);
 				jacob_sequence.push_back(3);
 				jacob_sequence.push_back(5);
+				jacob_sequence.push_back(11);
 			}
 			for ( size_t i = jacob_sequence.size(); i < n; i++)
 			{
 				jacob_sequence.push_back(jacob_sequence[i - 1] + 2 * jacob_sequence[i - 2]);
 			}
 		}
+
 		void jacob_insertion(std::vector<int>& main_chain, std::vector<int>& pend_chain)
 		{
-			std::vector<bool> inserted(pend_chain.size(), false);
-			std::vector<int>::iterator main_it;
-			for ( size_t i = 0; i < jacob_sequence.size(); i++)
-			{
-				if ( jacob_sequence[i] >= pend_chain.size())
-					break;
+			
+		}
 
-				int value  = pend_chain[jacob_sequence[i]];
-				main_it = std::lower_bound(main_chain.begin(), main_chain.end(), value);
-				main_chain.insert(main_it, value);
-				inserted[jacob_sequence[i]] = true;
-			}
-			for ( size_t i = 0; i < pend_chain.size(); i++)
+		void switch_units(std::vector<int>& nums, int first, int second, int unit_size)
+		{
+			vec_iterator first_it, second_it;
+			first_it = nums.begin() + first;
+			second_it = nums.begin() + second;
+			for ( int i = 0; i < unit_size; i++)
 			{
-				if ( !inserted[i] )
-				{
-					main_it = std::lower_bound(main_chain.begin(), main_chain.end(), pend_chain[i]);
-					main_chain.insert(main_it, pend_chain[i]);	
-				}
+				std::iter_swap(first_it, second_it);
+				--first_it;
+				--second_it;
 			}
+		} 
+
+		void ford_jhonson_sort(std::vector<int>& nums, int unit_size)
+		{
+			int possible_pairs = nums.size() / unit_size;
+			bool odd_exists = false;
+			if ( possible_pairs < 2)
+				return ;
+			if ( possible_pairs % 2 != 0)
+			{
+				possible_pairs--;
+				odd_exists = true;
+			}
+			for ( int i = unit_size - 1, comp = 0; comp < possible_pairs / 2; comp++, i += unit_size * 2)
+			{
+				//compare the biggest number of each unit to the next one if they are unsorted switch the whole units
+				if ( nums[i] > nums[i + unit_size])
+					switch_units(nums, i, i + unit_size, unit_size);
+			}
+			// recurse over the vector with double the unit size
+			ford_jhonson_sort(nums, unit_size * 2);
+
+			std::vector<int> main;
+			std::vector<int> pend;
+
+			//insert the first and the second element of the vector since we know b1 is less then a1
+			main.insert(main.begin(), nums.begin(), nums.begin() + (unit_size * 2));
+
+			vec_iterator b_element = nums.begin() + (unit_size * 2);
+			vec_iterator a_element = b_element + unit_size;
+			//insert all the big element of each pair of comparison in the main chain and the smallest of the pair into the pend chain the element can have a vairant size from 1 to ... they are not forcefully pairs
+			for ( int i = 2; i < possible_pairs; i+= 2)
+			{
+				main.insert(main.end(), a_element, a_element + unit_size);
+				pend.insert(pend.end(), b_element, b_element + unit_size);
+				a_element += unit_size;
+				b_element += unit_size;
+			}
+			if ( odd_exists )
+			{
+				// add the odd element whih couldnt find another unit to compare itself with , the element is situated after the last a element 
+				pend.insert(pend.end(), a_element, a_element + unit_size);
+			}
+			// the pairs of pairs of pairs ..... sort is now done , and the construction of the main and pend chain is done 
+			// we now need to insert the elements of the pend chain into the main chain using the jacob sthall sequence 
+			/* which goes as follows:
+				we iterate over the jacob sthall seuence first starting with 3 and then we substract it from the previous jacob sthal number 
+				this gives us how many elements should be pushed from the pend to the main
+				we start at the end of the result of the substraction meaning 3 -1 = 2 we start at the index 1 and work backwards insert 1 the insert 0
+				if the result of the substraction is bigger then how many elements are in the pend chain we break out
+				the insertion is done using binary search from the the begining of the nums up to the bound element 
+				the bound element is the matching pair of the pend element tha is first pointed to by the jacob sthall meaning the first one to be inserted each time we recalculate the jacob sthall number
+				if the pend element to be first inserted in the first iteration of the first insertion( jacob sthal 3) b3 then the bound is the matching one of that pair a3 
+				and since we already know that the main chain has b1 a1 a2 a3 then the index of the match is 3 which is the same as the jacob sthall number ,
+				this stays true for all elemnents if we ont push anything into the main chain , but since we are updating it with elements from the pend we have to adjust that wit ading to the bound 
+				how many elements we inserted
+				when inserting an element it could be supposed to be inserted above the bound by one meaning the bounf willb enow invalid so we need to move it back one place.
+			*/
+			
+
+
+			
 		}
 		
-		std::vector<int> ford_jhonson_sort( std::vector<int> nums)
+		std::vector<int> sort_numbers( std::vector<int> nums)
 		{
-			int n = nums.size();
-			if ( n == 1)
-				return nums;
-			std::vector<int> pend_chain;
-			std::vector<int> main_chain;
-			
-			/*
-				check if the vector has an odd number of elements keep the elements 
-				and set the flag to true so it is added to the main chain when it is formed
-			*/
-			bool odd_exists = false;
-			int odd_element; 
-			if ( n % 2 != 0)
-			{
-				n--;
-				odd_exists = true;
-				odd_element = nums[nums.size() - 1];
-				nums.pop_back();
-			}
-			for ( int i = 1; i < n; i+=2)
-			{
-				if ( nums[i] > nums[i - 1])
-				{
-					main_chain.push_back(nums[i]);
-					pend_chain.push_back(nums[i - 1]);
-				}
-				else if ( nums[i] <= nums[i - 1])
-				{
-					main_chain.push_back(nums[i - 1]);
-					pend_chain.push_back(nums[i]);
-				}
-			}
-			//add the last element of the incoming vector if it was odd sized
-			if ( odd_exists )
-				main_chain.push_back(odd_element);
-			std::cout << main_chain.size() << " " << pend_chain.size() << std::endl;
-			//recurse over the main chain untill it is down to the base case of one element which is sorted
-			main_chain = ford_jhonson_sort(main_chain);
-			//from the returned sorted main chain insert the pend chain elements into it using the jacob sthall sequence as indexes to which element to push into 
-			// the main chain, the insetion is done using binary search 
-			jacob_insertion(main_chain, pend_chain);
-			//at this point the main_chain is sorted and keeps on return up the stack until it goes back to the first call of ford jhoson and on each return the 
-			// corresponding pend chain element of that step are the pushed to the returned array
-			return main_chain;
-		}
-		std::vector<int> pmergeme(std::vector<int>& nums)
-		{
-			//generate the jacob sthal sequence up to half of the size of the given array since the first iteration of the ford jhonson will split them into two 
-			// that means that at most i will need half of the array size in jacob sthall. ( even less since it grows exponentially but whatever).
-			generate_jacob_sequence( nums.size() / 2 + 1);
-			return ford_jhonson_sort(nums);
+			generate_jacob_sequence(nums.size() / 3);
+			ford_jhonson_sort(nums, 1);
+			// for ( size_t i = 0 ; i < nums.size(); i++)
+			// {
+			// 	std::cout << ( i == 0? '\0' : ' ') << nums[i];
+			// }
+				// std::cout << std::endl;
+			return nums;
 		}
 };
 
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -3,15 +3,16 @@
 int main()
 {
 	PmergeMe sorter;
-	int arr[]  = { 6, 7, 9, 10, 11, 21, 22, 27, 28, 35, 36, 37};
+	int arr[]  = { 11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
 	std::vector<int> test( arr , arr + (sizeof(arr) / sizeof( arr[0])));
-	std::vector<int> ret = sorter.pmergeme(test);
-	for ( size_t i = 0 ; i < ret.size() ; i++)
-		std::cout << (i == 0 ? "": " ") << ret[i];
-	std::sort(test.begin() , test.end());
-	std::cout << std::endl;
-	for ( size_t i = 0 ; i < test.size() ; i++)
-		std::cout << (i == 0 ? "": " ") << test[i];
+	sorter.sort_numbers(test);
+	// std::vector<int> ret = sorter.pmergeme(test);
+	// for ( size_t i = 0 ; i < ret.size() ; i++)
+	// 	std::cout << (i == 0 ? "": " ") << ret[i];
+	// std::sort(test.begin() , test.end());
+	// std::cout << std::endl;
+	// for ( size_t i = 0 ; i < test.size() ; i++)
+	// 	std::cout << (i == 0 ? "": " ") << test[i];
 	
 	
 }
