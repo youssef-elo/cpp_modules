887fd23c5b0f966e2bc4a912dc0de4e6a7b9a0bb
youssef-elo <144487581+youssef-elo@users.noreply.github.com>
2025-09-28 12:15:34 +0100

    ford jhonson merge-insert sort

---
+++ cpp09/ex02/PmergeMe.cpp
@@ -0,0 +1,301 @@
+#include "PmergeMe.hpp"
+// PmergeMe::
+
+long PmergeMe::getjacob(long n)
+{
+	return round((pow(2, n + 1) + pow(-1, n)) / 3);
+}
+
+void PmergeMe::ford_jhonson_sort_vector(std::vector<int> &nums, int unit_size)
+{
+	int possible_pairs = nums.size() / unit_size;
+	bool odd_exists = false;
+	if (possible_pairs < 2)
+		return;
+	if (possible_pairs % 2 != 0)
+	{
+		possible_pairs--;
+		odd_exists = true;
+	}
+	for (int i = unit_size - 1, comp = 0; comp < possible_pairs / 2; comp++, i += unit_size * 2)
+	{
+		// compare the biggest number of each unit to the next one if they are unsorted switch the whole units
+		if (nums[i] > nums[i + unit_size])
+			switch_units(nums, i, i + unit_size, unit_size);
+	}
+	// recurse over the vector with double the unit size
+	ford_jhonson_sort_vector(nums, unit_size * 2);
+
+	std::vector<int> main;
+	std::vector<int> pend;
+	// serves as a simplified indexing of the biggest elements of each unit in order to do the binary search only on the biggest element of the chain
+	std::vector<int> unit_leader;
+
+	// insert the first and the second element of the vector since we know b1 is less then a1
+	main.insert(main.begin(), nums.begin(), nums.begin() + (unit_size * 2));
+	unit_leader.push_back(main[unit_size - 1]);
+	unit_leader.push_back(main[((2 * unit_size) - 1)]);
+	vec_iterator b_element = nums.begin() + (unit_size * 2);
+	if (possible_pairs > 2)
+	{
+		// insert all the big element of each pair of comparison in the main chain and the smallest of the pair into the pend chain the element can have a vairant size from 1 to ... they are not forcefully pairs
+		vec_iterator a_element = b_element + unit_size;
+		for (int i = 2; i < possible_pairs; i += 2)
+		{
+			unit_leader.push_back(*(a_element + (unit_size - 1)));
+			pend.insert(pend.end(), b_element, b_element + unit_size);
+			main.insert(main.end(), a_element, a_element + unit_size);
+			a_element += unit_size * 2;
+			b_element += unit_size * 2;
+		}
+	}
+	if (odd_exists)
+	{
+		// add the odd element whih couldnt find another unit to compare itself with , the element is situated after the last a element
+		vec_iterator odd_start = nums.begin() + (unit_size * possible_pairs);
+		pend.insert(pend.end(), odd_start, odd_start + unit_size);
+	}
+
+	// the pairs of pairs of pairs ..... sort is now done , and the construction of the main and pend chain is done
+	// we now need to insert the elements of the pend chain into the main chain using the jacob sthall sequence
+	/* which goes as follows:
+		we iterate over the jacob sthall seuence first starting with 3 and then we substract it from the previous jacob sthal number
+		this gives us how many elements should be pushed from the pend to the main
+		we start at the end of the result of the substraction meaning 3 -1 = 2 we start at the index 1 and work backwards insert 1 the insert 0
+		if the result of the substraction is bigger then how many elements are in the pend chain we break out
+		the insertion is done using binary search from the the begining of the nums up to the bound element
+		the bound element is the matching pair of the pend element tha is first pointed to by the jacob sthall meaning the first one to be inserted each time we recalculate the jacob sthall number
+		if the pend element to be first inserted in the first iteration of the first insertion( jacob sthal 3) b3 then the bound is the matching one of that pair a3
+		and since we already know that the main chain has b1 a1 a2 a3 then the index of the match is 3 which is the same as the jacob sthall number ,
+		this stays true for all elemnents if we ont push anything into the main chain , but since we are updating it with elements from the pend we have to adjust that wit ading to the bound
+		how many elements we inserted
+		when inserting an element it could be supposed to be inserted above the bound by one meaning the bound willb now invalid so we need to move it back one place.
+	*/
+
+	size_t i = 1;
+	size_t on_limit = 0;
+	size_t current_jacob;
+	size_t inserted_count = 0;
+	size_t previous_jacob = 1;
+	size_t number_of_insertions;
+
+	while (pend.size())
+	{
+		current_jacob = getjacob(i);
+		number_of_insertions = current_jacob - previous_jacob;
+		if (number_of_insertions * unit_size > pend.size())
+			break;
+		vec_iterator pend_leader = pend.begin() + (number_of_insertions * unit_size) - 1;
+
+		vec_iterator erase_pend = pend_leader;
+		while (number_of_insertions)
+		{
+			vec_iterator search_limit = unit_leader.begin() + current_jacob + inserted_count - on_limit;
+			// if ( search_limit > unit_leader.end())
+			// {
+			// 	// on_limit++;
+			// 	std::cout << (pend_leader - pend.begin()) << "size " << pend.size() << std::endl;
+			// 	std::cout << "unit size " << unit_size << " pend element " << *pend_leader << std::endl;
+			// 	search_limit = unit_leader.end();
+			// }
+			vec_iterator leader_it = std::upper_bound(unit_leader.begin(), search_limit, *pend_leader);
+			if (search_limit == leader_it)
+				on_limit++;
+			size_t insert_at = leader_it - unit_leader.begin();
+			unit_leader.insert(leader_it, *pend_leader);
+			vec_iterator main_insert = main.begin() + (insert_at * unit_size);
+
+			main.insert(main_insert, pend_leader - unit_size + 1, pend_leader + 1);
+			pend_leader -= unit_size;
+			number_of_insertions--;
+			inserted_count++;
+		}
+		pend.erase(pend.begin(), erase_pend + 1);
+		on_limit = 0;
+		previous_jacob = current_jacob;
+		i++;
+	}
+
+	// element might not all be inserted with the jacob sthall insertion style since the jacob number might be too big
+	// then we need to insert the remaining eements in reverese order
+	// to calculate the search limit for the each of the ramaining elements we use
+	// size_of_main - size_of_pend + index_of_current_pend. */
+	if (pend.size())
+	{
+		vec_iterator pend_leader = pend.end() - 1;
+
+		int pend_count = pend.size() / unit_size;
+		for (int i = pend.size() - 1; i >= 0; i -= unit_size)
+		{
+			vec_iterator search_limit = unit_leader.begin() + ((unit_leader.size() - pend_count) + (i / unit_size) + odd_exists);
+			if (search_limit > unit_leader.end())
+				search_limit = unit_leader.end();
+			vec_iterator leader_it = std::upper_bound(unit_leader.begin(), search_limit, *pend_leader);
+			int insert_at = leader_it - unit_leader.begin();
+
+			unit_leader.insert(leader_it, *pend_leader);
+			main.insert(main.begin() + (insert_at * unit_size), pend_leader - unit_size + 1, pend_leader + 1);
+			pend_leader -= unit_size;
+		}
+	}
+	for (size_t i = 0; i < main.size(); i++)
+	{
+		nums[i] = main[i];
+	}
+}
+
+
+void PmergeMe::ford_jhonson_sort_deque(std::deque<int> &nums, int unit_size)
+{
+	int possible_pairs = nums.size() / unit_size;
+	bool odd_exists = false;
+	if (possible_pairs < 2)
+		return;
+	if (possible_pairs % 2 != 0)
+	{
+		possible_pairs--;
+		odd_exists = true;
+	}
+	for (int i = unit_size - 1, comp = 0; comp < possible_pairs / 2; comp++, i += unit_size * 2)
+	{
+		if (nums[i] > nums[i + unit_size])
+			switch_units(nums, i, i + unit_size, unit_size);
+	}
+	ford_jhonson_sort_deque(nums, unit_size * 2);
+
+	std::deque<int> main;
+	std::deque<int> pend;
+	std::deque<int> unit_leader;
+
+	main.insert(main.begin(), nums.begin(), nums.begin() + (unit_size * 2));
+	unit_leader.push_back(main[unit_size - 1]);
+	unit_leader.push_back(main[((2 * unit_size) - 1)]);
+	deq_iterator b_element = nums.begin() + (unit_size * 2);
+	int step = unit_size * 2;
+	if (possible_pairs > 2)
+	{
+		deq_iterator a_element = b_element + unit_size;
+		for (int i = 2; i < possible_pairs; i += 2)
+		{
+			if (std::distance(a_element, nums.end()) > step)
+			{
+				unit_leader.push_back(*(a_element + (unit_size - 1)));
+				main.insert(main.end(), a_element, a_element + unit_size);
+				a_element += step;
+			}
+			if (std::distance(b_element, nums.end()) > step)
+			{
+				pend.insert(pend.end(), b_element, b_element + unit_size);
+				b_element += step;
+			}
+		}
+	}
+	if (odd_exists)
+	{
+		deq_iterator odd_start = nums.begin() + (unit_size * possible_pairs);
+		pend.insert(pend.end(), odd_start, odd_start + unit_size);
+	}
+
+	size_t i = 1;
+	size_t on_limit = 0;
+	size_t current_jacob;
+	size_t inserted_count = 0;
+	size_t previous_jacob = 1;
+	size_t number_of_insertions;
+
+	while (pend.size())
+	{
+		current_jacob = getjacob(i);
+		number_of_insertions = current_jacob - previous_jacob;
+		if (number_of_insertions * unit_size > pend.size())
+			break;
+		deq_iterator pend_leader = pend.begin() + (number_of_insertions * unit_size) - 1;
+
+		deq_iterator erase_pend = pend_leader;
+		while (number_of_insertions)
+		{
+			deq_iterator search_limit = unit_leader.begin() + current_jacob + inserted_count - on_limit;
+			deq_iterator leader_it = std::upper_bound(unit_leader.begin(), search_limit, *pend_leader);
+			if (search_limit == leader_it)
+				on_limit++;
+			size_t insert_at = leader_it - unit_leader.begin();
+			unit_leader.insert(leader_it, *pend_leader);
+			deq_iterator main_insert = main.begin() + (insert_at * unit_size);
+
+			main.insert(main_insert, pend_leader - unit_size + 1, pend_leader + 1);
+			pend_leader -= unit_size;
+			number_of_insertions--;
+			inserted_count++;
+		}
+		pend.erase(pend.begin(), erase_pend + 1);
+		on_limit = 0;
+		previous_jacob = current_jacob;
+		i++;
+	}
+
+	if (pend.size())
+	{
+		deq_iterator pend_leader = pend.end() - 1;
+
+		int pend_count = pend.size() / unit_size;
+		for (int i = pend.size() - 1; i >= 0; i -= unit_size)
+		{
+			deq_iterator search_limit = unit_leader.begin() + ((unit_leader.size() - pend_count) + (i / unit_size) + odd_exists);
+			if (search_limit > unit_leader.end())
+				search_limit = unit_leader.end();
+			deq_iterator leader_it = std::upper_bound(unit_leader.begin(), search_limit, *pend_leader);
+			int insert_at = leader_it - unit_leader.begin();
+
+			unit_leader.insert(leader_it, *pend_leader);
+			main.insert(main.begin() + (insert_at * unit_size), pend_leader - unit_size + 1, pend_leader + 1);
+			pend_leader -= unit_size;
+		}
+	}
+	for (size_t i = 0; i < main.size(); i++)
+	{
+		nums[i] = main[i];
+	}
+	main.clear();
+}
+
+void PmergeMe::sort_numbers(std::vector<int> nums_vec, std::deque<int> nums_deq)
+{
+	size_t n = nums_deq.size();
+	size_t n_vec = nums_vec.size();
+	if (is_sorted(nums_vec))
+	{
+		std::cout << "Integer sequence is already sorted." << std::endl;
+		return;
+	}
+
+	std::cout << "Before : " << std::endl;
+	for (size_t i = 0; i < nums_vec.size(); i++)
+		std::cout << (i == 0 ? "" : " ") << nums_vec[i];
+	std::cout << std::endl;
+
+	clock_t start_vec = clock();
+	ford_jhonson_sort_vector(nums_vec, 1);
+	clock_t end_vec = clock();
+
+	clock_t start_deq = clock();
+	ford_jhonson_sort_deque(nums_deq, 1);
+	clock_t end_deq = clock();
+
+	if (!is_sorted(nums_deq) || n != nums_deq.size())
+		std::cerr << "Deque failed to sort its elements." << std::endl;
+	if (!is_sorted(nums_vec) || n_vec != nums_vec.size())
+		std::cerr << "Deque failed to sort its elements." << std::endl;
+
+	std::cout << "After: " << std::endl;
+	for (size_t i = 0; i < nums_vec.size(); i++)
+		std::cout << (i == 0 ? "" : " ") << nums_vec[i];
+
+	std::cout << std::endl;
+
+	double vec_time = (double)(end_vec - start_vec) / (double)CLOCKS_PER_SEC;
+	double deq_time = (double)(end_deq - start_deq) / (double)CLOCKS_PER_SEC;
+
+	std::cout << std::fixed << std::setprecision(6) << std::endl
+			  << "Time to process a range of " << nums_vec.size() << " with std::vector : " << vec_time << " s." << std::endl;
+	std::cout << std::fixed << std::setprecision(6) << "Time to process a range of " << nums_deq.size() << " with std::deque : " << deq_time << " s." << std::endl;
+}
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,19 +1,9 @@
 #include "PmergeMe.hpp"
 
-template<typename Container>
-void print_container(const Container& c)
-{
-	for ( size_t i = 0; i < c.size(); i++)
-		std::cout << ( i == 0 ? "" : " ") << c[i];
-	std::cout << std::endl;
-}
-
-
 bool parse_numbers( char **av, int ac, std::vector<int>& nums_vec, std::deque<int>& nums_deq)
 {
 	std::stringstream ss;
 	int tmp;
-	std::string stmp;
 	
 	for ( int i = 0; i < ac; i++)
 	{
@@ -21,7 +11,7 @@ bool parse_numbers( char **av, int ac, std::vector<int>& nums_vec, std::deque<in
 		ss >> tmp;
 		if ( ss.fail() || !ss.eof() || tmp < 0)
 		{
-			std::cerr << "Arguments should be a positive sequence only digits, no negative numbers !" << std::endl;
+			std::cerr << "Arguments should be a positive integer sequence only digits, only leading white spaces are allowed, no trailing, no negative numbers !" << std::endl;
 			return false;
 		}
 		nums_vec.push_back(tmp);
@@ -33,20 +23,16 @@ bool parse_numbers( char **av, int ac, std::vector<int>& nums_vec, std::deque<in
 
 int main(int ac, char **av)
 {
+	if ( ac == 1)
+	{
+		std::cerr << "No arguments!" << std::endl;
+		return 1;
+	}
 	std::vector<int> nums_vec;
 	std::deque<int> nums_deq;
 	PmergeMe sorter;
 
 	if ( !parse_numbers(av + 1, ac - 1, nums_vec, nums_deq) )
 		return 1;
-	
-	// int arr[] = {3, 1, 4};
-	// int arr[] = {42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
-	// int arr[] = {45, 12, 23, 89, 56, 77, 34, 90, 11, 65, 1000, 540, 2458, 54, 21, 32, 87, 15, 33, 29, 48, 73, 99, 101, 2, 6};
-	// int arr[]  = { 11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
-	// int arr[] = {100, 99, 98, 97, 96, 95, 94, 93, 92, 91};
-	// std::vector<int> test( arr , arr + (sizeof(arr) / sizeof( arr[0])));
-	// std::vector<int> ret =  sorter.sort_numbers(test);
 	sorter.sort_numbers(nums_vec, nums_deq);
-	
 }
